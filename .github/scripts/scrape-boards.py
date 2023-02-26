49b52acd759a5a1c3e1c575fc6ba25234bd6434f
Apacelus <46828271+apacelus@users.noreply.github.com>
2023-02-26 23:38:48 +0100

    Add devices-list.json + add readme

--- .github/scripts/scrape-boards.py
+++ .github/scripts/scrape-boards.py
@@ -1,3 +1,4 @@
+import contextlib
 import json
 
 import requests
@@ -14,19 +15,18 @@ devices_dict = {}
 
 # scan all rows starting from the second row
 for table in devices_table.find_all("tr")[1:]:
-    temp_dict = {}
     row = table.text.split("\n")[2:11]  # get relevant data from the row
-    temp_dict["code_name"] = row[2]
-    temp_dict["family_name"] = row[3]
-    temp_dict["arch"] = row[7]
-    temp_dict["cpu_gen"] = row[8]
+    temp_dict = {
+        "code_name": row[2],
+        "family_name": row[3],
+        "arch": row[7],
+        "cpu_gen": row[8],
+    }
     device_key = row[1].replace(row[0].lower().capitalize(), "").strip()  # remove brand name from device name
     device_key = device_key.replace(row[0].upper(), "").strip()  # remove brand name from device name
-    try:
+    with contextlib.suppress(IndexError):
         if device_key[0] == "-":
             device_key = device_key[1:]
-    except IndexError:
-        pass  # some devices dont have a name
     try:
         devices_dict[row[0].capitalize().strip()][device_key] = temp_dict
     except KeyError:
@@ -35,3 +35,12 @@ print(devices_dict)
 devices_dict.pop("")  # remove broken entry
 with open("./device-support/devices-autogen.json", "w") as f:
     json.dump(devices_dict, f)
+
+# combine autogen and extra into one json file
+with open("./device-support/devices-autogen.json", "r") as f:
+    autogen = json.load(f)
+with open("./device-support/devices-extra.json", "r") as f:
+    extra = json.load(f)
+autogen.update(extra)
+with open("./device-support/devices-list.json", "w") as f:
+    json.dump(autogen, f)
