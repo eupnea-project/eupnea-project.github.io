f99125d8346f2834fbdd179382337bae84c6d110
doodlezucc <electerminator@gmail.com>
2023-03-18 17:50:44 +0100

    added styling to faq entries

--- components/FAQEntry.vue
+++ components/FAQEntry.vue
@@ -8,22 +8,54 @@ defineProps({
 </script>
 
 <template>
-    <details class="details custom-block faq-entry" :open="true">
+    <details class="faq-entry">
         <summary>
-            <h3>{{ question }}</h3>
+            {{ question }}
+            <!-- <h3>{{ question }}</h3> -->
         </summary>
 
-        <slot></slot>
+        <div class="faq-answer">
+            <slot></slot>
+        </div>
     </details>
 </template>
 
 <style>
-.faq-entry summary {
-    font-size: 20px;
+.faq-entry {
+    margin: 16px 0;
+    color: var(--vp-custom-block-details-text);
+    background-color: var(--vp-custom-block-details-bg);
+    transition: 0.3s;
+
+    border: 1px solid var(--vp-custom-block-details-border);
+    border-radius: 8px;
 }
 
-.faq-entry h3 {
-    display: inline;
+.faq-entry summary {
+    font-size: 20px;
+    cursor: pointer;
+    margin: 0;
+    transition: 0.1s;
+    padding: 16px 20px;
+}
+.faq-entry summary:hover {
+    color: var(--vp-c-text-1);
+}
+
+.faq-answer {
+    padding: 0 20px;
+}
+
+.faq-answer > :first-child {
     margin-top: 0;
+}
+
+.faq-entry[open] {
+    color: var(--vp-c-text-1);
+    border-color: var(--vp-custom-block-tip-border);
+}
+.faq-entry[open] summary {
+    color: var(--vp-c-text-1);
+    border-color: var(--vp-custom-block-tip-border);
 }
 </style>
